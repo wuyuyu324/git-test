58bd88e10d0436262d03452a02e53d5cc513e9d5
wxy <1045877102@qq.com>
2021-07-12 14:18:17 +0800

    first commit

---
+++ src/main/java/com/wxy/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.wxy.git;
+
+/**
+ * @author wxy
+ * @create 2021-07-12 14:15
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
