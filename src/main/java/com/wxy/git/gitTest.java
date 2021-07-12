4b0400d82d72fd1639836ba20279b3b30f80807d
wxy <1045877102@qq.com>
2021-07-12 14:30:32 +0800

    second commit

--- src/main/java/com/wxy/git/gitTest.java
+++ src/main/java/com/wxy/git/gitTest.java
@@ -7,5 +7,6 @@ package com.wxy.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
     }
 }
