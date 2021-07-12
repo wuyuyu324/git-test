ca4506d4ef481dc94e68f4c9965b092b2196dca0
wxy <1045877102@qq.com>
2021-07-12 14:40:25 +0800

    master commit2

--- src/main/java/com/wxy/git/gitTest.java
+++ src/main/java/com/wxy/git/gitTest.java
@@ -9,5 +9,6 @@ public class gitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git4");
+        System.out.println("master test");
     }
 }
