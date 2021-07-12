6918ddd6d30f9f06e2474ffca1277c274f20cf66
wuyuyu324 <45456225+wuyuyu324@users.noreply.github.com>
2021-07-12 15:09:41 +0800

    pulltest

--- src/main/java/com/wxy/git/gitTest.java
+++ src/main/java/com/wxy/git/gitTest.java
@@ -12,5 +12,6 @@ public class gitTest {
         System.out.println("master test");
         System.out.println("hot-fix");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
